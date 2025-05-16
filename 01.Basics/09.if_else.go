c73b19b492f8641e3a9c3c59bc9e2ec0af7442c1
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2025-05-16 22:02:46 +0530

    input validation with if-else

--- 01.Basics/09.if_else.go
+++ 01.Basics/09.if_else.go
@@ -1,24 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func if_else() {
 	var n int
 	fmt.Print("Enter a number: ")
 	fmt.Scan(&n)
-	// if block
+
+	// 1. if block
 	if n%2 == 0 {
 		fmt.Printf("%v is a Even number\n", n)
 	}
 
-	// if-else block
+	// 2. if-else block
 	if n%2 == 0 {
 		fmt.Printf("%v is a Even number\n", n)
 	} else {
 		fmt.Printf("%v is a Odd number\n", n)
 	}
 
-	// else-if block
+	// 3. else-if block
 
 	if n > 0 {
 		fmt.Printf("%v is a Positive number\n", n)
@@ -26,6 +30,31 @@ func if_else() {
 		fmt.Printf("%v is a Negative number\n", n)
 	} else {
 		fmt.Printf("%v is neither postive nor negative\n", n)
+	}
+
+	// 4. if-else can be used input validation
+
+	var firstName string
+	var lastName string
+	var email string
+	fmt.Print("Enter a first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter a last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter a email: ")
+	fmt.Scan(&email)
+
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@") && len(email) >= 4
+
+	if isValidName && isValidEmail {
+		fmt.Printf("Hey %v %v! \nWelcome Aboard!\nYour email is: %v\n", firstName, lastName, email)
+	} else {
+		if !isValidName {
+			fmt.Println("Entered first or last should be more than of length 2")
+		} else if !isValidEmail {
+			fmt.Println("email must be of length more than 4 and contains @")
+		}
 	}
 
 	fmt.Println()
