8f31c718490ab010f1cfee68871cb61102f49146
Prince Bansal <75045465+princebansal7@users.noreply.github.com>
2025-05-19 12:56:57 +0530

    booking app logic with go basics

---
+++ 02.Booking-app/00-main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// bookingVariables()
+	// bookingArrays()
+	// bookingLoops()
+	// bookingIfElse()
+	// bookingFunctions()
+	// bookingMaps()
+	bookingStructures()
+}
